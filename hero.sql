e6850f1cb5a10d1087dded0ac93fe6a036f3e8af
Eiman <esa031302@gmail.com>
2023-11-08 20:04:15 +0800

    feat/update-delete-inactive

--- hero.sql
+++ hero.sql
@@ -156,3 +156,11 @@ VALUES
     (8, 8, 108),  -- Fanny has Thunder Belt
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
+
+    --4 Update status and Delete
+ UPDATE heroes
+SET state = 'inactive'
+WHERE hero_id = 1;
+
+DELETE FROM items
+WHERE hero_id = 1;
